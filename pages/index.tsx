caeabf376c4c56c9bdc25afd1ec327cab7e6ffea
juice me <shawn@hatswapcity.com>
2022-03-23 23:45:55 -0400

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <Image
-      id="backgroundImage"
-      src="/images/background.svg"
-      width="100vw"
-      height="171vw"
-      layout="responsive"
-    />
+    <svg id="panel" width="100vw" height="171vw" viewBox="0 0 2304 3955">
+      <image id="backgrond" href="images/background.svg" width="2304" height="3955"></image>
+      <image id="discord" href="images/discord.svg" x="400" y="3126" width="1512" height="152" onClick={() => {
+        window.open("https://discord.gg/DZjRR5TS")
+      }}></image>
+      <image id="telegram" href="images/telegram.svg" x="400" y="3340" width="1512" height="152" onClick={() => {
+        window.open("https://twitter.com/war_cat_army")
+      }}></image>
+    </svg>
   )
 }
 
