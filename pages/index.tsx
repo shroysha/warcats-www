1c8c06ea9ec6b8b4350d1af9f06e8ee17a87c754
juice me <shawn@hatswapcity.com>
2022-03-30 22:42:13 -0400

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
+    
     <svg id="panel" width="100vw" height="171vw" viewBox="0 0 2304 3955">
+      <Head>
+        <title>War Cats</title>
+      </Head>,
       <image id="backgrond" href="images/background.svg" width="2304" height="3955"></image>
       <image id="discord" href="images/discord.svg" x="400" y="3126" width="1512" height="152" onClick={() => {
         window.open("https://discord.gg/DZjRR5TS")
