c8ba3715cd1b6819eb7ef7f86ed6461c21c8d21a
juice me <shawn@hatswapcity.com>
2022-05-03 23:14:57 -0400

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,14 +13,14 @@ interface HomeProps {
 
 const Home: NextPage = (props: HomeProps) => {
   return <>
-     {/* <Head>
+     <Head>
         <title>War Cats</title>
       </Head>
       <WarCatPanel width={PAGE_WIDTH} height={PAGE_HEIGHT}>
           <SocialButton x='468' y='3043' href={TWITTER_HREF} imageHref={IMAGE_TWITTER_HREF}/>
           <SocialButton x='455' y='3178' href={DISCORD_HREF} imageHref={IMAGE_DISCORD_HREF}/>
           <BuyButton x="400" y="3410"/>
-      </WarCatPanel> */}
+      </WarCatPanel>
   </>
 }
 
